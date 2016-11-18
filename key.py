062473c20e59f259d38edcd79e22d0d215b8f52f
Steve Gregg <sgregg85@gmail.com>
2016-11-17 23:58:32 -0500

    Add file to store API Access keys

---
+++ key.py
@@ -0,0 +1,4 @@
+consumer_key = '' # Enter your values here
+consumer_secret = '' # Enter your values here
+access_token = '' # Enter your values here
+access_token_secret = '' # Enter your values here
