a6f3cd0e8f72e48f3a026d1f45abb80b21a25094
Steve Gregg <sgregg85@gmail.com>
2016-11-19 12:30:52 -0500

    Added comments

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import key
+import key #key.py file with keys from twitter
 import tweepy
 import requests
 import json
@@ -13,23 +13,24 @@ api = tweepy.API(auth)
 def getQuote():
 	url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json'#p&jsonp=?'
 	response = requests.get(url)
-	if (response.ok):
+	if (response.ok): #if we get a valid response from server
 		try:
+			#convert response to a json object
 			jData = json.loads(response.content.decode('utf-8'))
 			quote = (jData['quoteText'])
-			if(jData['quoteAuthor'] == ''):
+			if(jData['quoteAuthor'] == ''): #if no author, use Anonymous
 				author = "Anonymous"
 			else:
 				author = jData['quoteAuthor']
 			tweet = quote + " -" + author
-			if(len(tweet) > 160):
+			if(len(tweet) > 160): #if the tweet is more than 160 characters, we need another quote
 				print("Quote too long")
 				getQuote()
 			api.update_status(tweet)
-		except ValueError:
+		except ValueError: #try again if we get a valuerror
 			getQuote()
 	else:
-		response.raise_for_status()
+		response.raise_for_status() #print error from server
 
 	
 
